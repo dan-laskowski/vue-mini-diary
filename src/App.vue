d0764d009aec2e3634a9de1e2bcc1d6dad4556b6
dan-laskowski <daniel.laskowski94@gmail.com>
2021-12-06 17:06:05 +0100

    initialize the Supabase client

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="min-h-full font-Taviraj box-border">
+  <div v-if="appReady" class="min-h-full font-Taviraj box-border">
     <router-view />
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+import { supabase } from './supabase/index';
+import store from './store/index';
+export default {
+  setup() {
+    const appReady = ref(null);
+
+    const user = supabase.auth.user();
+
+    if (!user) {
+      appReady.value = true;
+    }
+
+    supabase.auth.onAuthStateChange((_, session) => {
+      store.methods.setUser(session);
+      appReady.value = true;
+    });
+
+    return { appReady };
+  },
+};
 </script>
 
 <style>
---
+++ src/supabase/index.js
@@ -0,0 +1,8 @@
+// helper file to initialize the Supabase client
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.VUE_APP_SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
