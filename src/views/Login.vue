2d2f21935f8eb88f894ecc4bcb7015e680788ea9
dan-laskowski <daniel.laskowski94@gmail.com>
2021-12-09 18:50:38 +0100

    add logic to login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,14 +7,17 @@
         <img
           class="mx-auto h-16"
           src="../assets/images/logo.png"
-          alt="Workflow"
+          alt="Notepad"
         />
         <h2 class="mt-6 text-center text-3xl font-extrabold text-greyish">
           Sign up to your account
         </h2>
       </div>
-      <form class="mt-8 space-y-6" @submit.prevent="registerUser">
-        <p v-if="{ errorMessage }" class="text-red-500">{{ errorMessage }}</p>
+      <p v-if="{ statusMessage }" class="text-green-600">{{ statusMessage }}</p>
+      <p v-if="{ errorMessage }" class="text-red-500">{{ errorMessage }}</p>
+
+      <!-- Password form -->
+      <form class="mt-8 space-y-6" @submit.prevent="loginUser">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
@@ -33,29 +36,18 @@
           <div>
             <label for="password" class="sr-only">Password</label>
             <input
+              v-if="!linkForm"
               id="password"
               name="password"
               type="password"
               autocomplete="current-password"
-              required=""
+              required="linkForm"
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Password"
               v-model="password"
             />
           </div>
         </div>
-        <div>
-          <router-link
-            class="text-sm mt-6 text-center"
-            :to="{ name: 'Register' }"
-          >
-            Don't have an account?
-            <span class="font-medium text-bluish hover:text-blue-500"
-              >Register</span
-            >
-          </router-link>
-        </div>
-
         <div>
           <button
             type="submit"
@@ -67,17 +59,30 @@
                 aria-hidden="true"
               />
             </span>
-            Sign up
+            {{ linkForm ? `Send me a magic link` : `Sign up` }}
           </button>
         </div>
       </form>
+
+      <div>
+        <router-link
+          class="text-sm mt-6 text-center"
+          :to="{ name: 'Register' }"
+        >
+          Don't have an account?
+          <span class="font-medium text-bluish hover:text-blue-500"
+            >Register</span
+          >
+        </router-link>
+      </div>
       <div class="flex items-center">
         <div class="flex-grow bg bg-gray-300 h-0.5"></div>
         <div class="flex-grow-0 mx-5 text dark:text-white">or</div>
         <div class="flex-grow bg bg-gray-300 h-0.5"></div>
       </div>
       <button
-        type="submit"
+        @click="switchForms"
+        type="button"
         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
@@ -86,7 +91,7 @@
             aria-hidden="true"
           />
         </span>
-        Login with a magic link
+        {{ linkForm ? 'Login with a password' : 'Login with a magic link' }}
       </button>
     </div>
   </div>
@@ -105,8 +110,10 @@ export default {
       email: ref(null),
       password: ref(null),
     };
+    const statusMessage = ref(null);
     const errorMessage = ref(null);
     const linkForm = ref(null);
+    linkForm.value = false;
 
     //Extracting multiple properties by object deconstruction
     const { email, password } = formValues;
@@ -119,21 +126,30 @@ export default {
           password: password.value,
         });
         if (error) throw error;
-        router.push({ name: 'Diary' });
+        !linkForm.value
+          ? router.push({ name: 'Diary' })
+          : (statusMessage.value = `Magic link send! Check out your inbox`);
       } catch (error) {
-        errorMessage.value = `Error: ${error.errorMessage}`;
+        errorMessage.value = `Error: ${error.message}`;
         setTimeout(() => {
           errorMessage.value = null;
         }, 5000);
       }
     };
 
+    const switchForms = () => {
+      linkForm.value = !linkForm.value;
+      password.value = null;
+    };
+
     return {
       email,
       password,
       errorMessage,
+      statusMessage,
       loginUser,
       linkForm,
+      switchForms,
       LockClosedIcon,
     };
   },
