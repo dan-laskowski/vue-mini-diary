d9f11e5d80b56a705d120141ea301697e744e52c
dan-laskowski <daniel.laskowski94@gmail.com>
2021-12-13 23:41:34 +0100

    add text editor to the projecy

--- src/views/Diary.vue
+++ src/views/Diary.vue
@@ -1,14 +1,18 @@
 <template>
-  <DatePicker v-model="date" />
-  <h2>{{ date.getDate() }}</h2>
+  <div class="grid grid-cols-2">
+    <DatePicker v-model="date" />
+    <Tiptap />
+    <h2>{{ date.getDate() }}</h2>
+  </div>
 </template>
 
 <script>
 import { DatePicker } from 'v-calendar';
+import Tiptap from '../components/Tiptap.vue';
 import { ref } from 'vue';
 export default {
   name: 'Diary',
-  components: { DatePicker },
+  components: { DatePicker, Tiptap },
   setup() {
     const date = ref(new Date());
 
