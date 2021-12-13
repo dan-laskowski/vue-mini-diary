2ad3bb225aacdf529cd9dd90ff2d2f97a817a6aa
dan-laskowski <daniel.laskowski94@gmail.com>
2021-12-13 18:22:26 +0100

    import calendar component to the project

--- src/views/Diary.vue
+++ src/views/Diary.vue
@@ -1,10 +1,19 @@
 <template>
-  <h1>Diary</h1>
+  <DatePicker v-model="date" />
+  <h2>{{ date.getDate() }}</h2>
 </template>
 
 <script>
+import { DatePicker } from 'v-calendar';
+import { ref } from 'vue';
 export default {
   name: 'Diary',
+  components: { DatePicker },
+  setup() {
+    const date = ref(new Date());
+
+    return { date };
+  },
 };
 </script>
 
