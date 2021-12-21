917a94b65f03fc5edb67ce1be2509fb1e77bd9e7
Daniel <daniel.laskowski94@gmail.com>
2021-12-21 11:56:32 +0100

    add update note functionality

--- src/views/Diary.vue
+++ src/views/Diary.vue
@@ -4,12 +4,12 @@
   >
     <div class="grid sm:grid-cols-2 sm:gap-14 grid-cols-1 gap-4">
       <div>
-        <DatePicker @click="getDayNote" class="font-sans mb-4" v-model="date" />
+        <DatePicker @click="getNote" class="font-sans mb-4" v-model="date" />
       </div>
       <div
-        class="flex flex-col justify-between border-gray-100 rounded-md border-2"
+        class="flex flex-col justify-between border-gray-100 rounded-md border-2 max-w-xs"
       >
-        <Tiptap v-model="content" @save-content="saveContent" />
+        <Tiptap v-model="content" @save-content="setNote" />
       </div>
       <h2>
         {{ `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}` }}
@@ -33,37 +33,8 @@ export default {
     const content = ref('');
     const user_id = store.state.user.id;
     console.log(moment().format());
-    const saveContent = async () => {
-      try {
-        const { error } = await supabase.from('notes').insert(
-          {
-            user_id,
-            note: content.value,
-            inserted_at: moment().format('L'),
-          },
-          { returning: 'minimal' }
-        );
-        if (error) throw error;
-        console.log('note added');
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    const getData = async () => {
-      try {
-        const { data: notes, error } = await supabase
-          .from('notes')
-          .select('note')
-          .eq('user_id', user_id);
-        if (error) throw error;
-        console.log(notes);
-        content.value = notes[0].note;
-      } catch (error) {
-        console.log(error);
-      }
-    };
 
-    const getDayNote = async () => {
+    const setNote = async () => {
       try {
         const { data: note, error } = await supabase
           .from('notes')
@@ -76,15 +47,68 @@ export default {
             }/${date.value.getDate()}/${date.value.getFullYear()}`
           );
         if (error) throw error;
-        content.value = note.length ? note[0].note : 'brak';
+        if (!note.length) {
+          try {
+            const { error } = await supabase.from('notes').insert(
+              {
+                user_id,
+                note: content.value,
+                inserted_at: moment().format('L'),
+              },
+              { returning: 'minimal' }
+            );
+            if (error) throw error;
+            console.log('note added');
+          } catch (error) {
+            console.log(error);
+          }
+        } else {
+          try {
+            const { error } = await supabase
+              .from('notes')
+              .update({
+                note: content.value,
+              })
+              .eq('user_id', user_id)
+              .eq(
+                'inserted_at',
+                `${
+                  date.value.getMonth() + 1
+                }/${date.value.getDate()}/${date.value.getFullYear()}`
+              );
+
+            if (error) throw error;
+            console.log('note updated');
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    const getNote = async () => {
+      try {
+        const { data: note, error } = await supabase
+          .from('notes')
+          .select('note')
+          .eq('user_id', user_id)
+          .eq(
+            'inserted_at',
+            `${
+              date.value.getMonth() + 1
+            }/${date.value.getDate()}/${date.value.getFullYear()}`
+          );
+        if (error) throw error;
+        console.log('note got');
+        content.value = note.length ? note[0].note : '';
       } catch (error) {
         console.log(error);
       }
     };
-    getData();
-    return { date, content, saveContent, getData, getDayNote };
+    getNote();
+    return { date, content, setNote, getNote };
   },
 };
 </script>
-
-<style></style>
