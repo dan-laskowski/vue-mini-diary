3ba23dd1f11a5c3d0ce510fc594906fde58a0415
dan-laskowski <daniel.laskowski94@gmail.com>
2022-01-12 23:47:51 +0100

    remove unused asset

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -91,10 +91,6 @@
             class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-bluish hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <LockClosedIcon
-                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                aria-hidden="true"
-              />
             </span>
             Sign up
           </button>
@@ -107,7 +103,6 @@
 <script>
 import { ref } from 'vue';
 import { errorMessage, handleSignup } from '../vuetils/useAuth';
-import { LockClosedIcon } from '@heroicons/vue/solid';
 export default {
   name: 'Register',
   setup() {
@@ -123,7 +118,6 @@ export default {
       confirmPassword,
       errorMessage,
       handleSignup,
-      LockClosedIcon,
     };
   },
 };
